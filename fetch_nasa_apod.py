2089ca6747bfd9e997809a71f8c318541d06729d
Al-Evst <al.evst96@gmail.com>
2025-03-01 19:58:50 +0100

    Update fetch_nasa_apod.py

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -4,19 +4,23 @@ import argparse
 from urllib.parse import urlparse
 from utils import get_image_extension, download_image 
 
-def fetch_nasa_apod(api_key: str, count: int, save_dir: str):
+def fetch_nasa_apod(api_key: str, count: int, dir_save: str):
     url = "https://api.nasa.gov/planetary/apod"
     params = {"api_key": api_key, "count": count}
     response = requests.get(url, params=params)
     response.raise_for_status()
     images = response.json()
-    os.makedirs(save_dir, exist_ok=True)
+    os.makedirs(dir_save, exist_ok=True)
+    
     for i, image_data in enumerate(images):
-        if "url" in image_data:
-            image_url = image_data["url"]
+        if image_data.get("media_type") == "image": 
+            image_url = image_data.get("hdurl") or image_data.get("url")
             ext = get_image_extension(image_url)
-            image_name = os.path.join(save_dir, f"nasa_apod_{i}{ext}")
+            image_name = os.path.join(dir_save, f"nasa_apod_{i}{ext}")
             download_image(image_url, image_name)
+            print(f"Downloaded: {image_name}")
+        else:
+            print(f"Skipping non-image content: {image_data.get('media_type')} - {image_data.get('url')}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
