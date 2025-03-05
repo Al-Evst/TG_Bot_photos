72c9e7c30ab3a6715cb57cfdb00011e8b1a4df19
Al-Evst <al.evst96@gmail.com>
2025-03-05 20:31:24 +0100

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -4,17 +4,17 @@ from urllib.parse import urlparse
 import argparse
 from utils import download_image
 
-def fetch_spacex_images(launch_id: str = "latest", dir_save: str = "images"):
+def fetch_spacex_images(launch_id: str = "latest", output_path: str = "images"):
     
     url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
     photos = data.get('links', {}).get('flickr', {}).get('original', [])
-    os.makedirs(dir_save, exist_ok=True)
+    os.makedirs(output_path, exist_ok=True)
     for i, photo in enumerate(photos, start=1):
         filename = f"spacex_{i}.jpg"
-        path_save = os.path.join(dir_save, filename)
+        path_save = os.path.join(output_path, filename)
         download_image(photo, path_save)
 
 if __name__ == "__main__":
